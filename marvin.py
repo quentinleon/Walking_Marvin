da3c4c24ea551d1163d49798b5c6c2aeb8c1ede6
Quentin Leon <qwolf1999@gmail.com>
2019-06-06 15:24:28 -0700

    structures

--- marvin.py
+++ marvin.py
@@ -1,14 +1,27 @@
 import gym
+import structures
 env = gym.make('Marvin-v0')
 
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
+print("Action space info")
+print(env.action_space)
+print(env.action_space.high)
+print(env.action_space.low)
+
+print()
+print("Observation space info")
+print(env.observation_space)
+print(env.observation_space.high)
+print(env.observation_space.low)
+
+for i_episode in range(5):
+	observation = env.reset()
+	t = 0
+	while True: #t in range(100):
+		t += 1
+		env.render()
+		action = env.action_space.sample()
+		observation, reward, done, info = env.step(action)
+		if done:
+			print("Episode finished after {} timesteps".format(t+1))
+			break
 env.close()
---
+++ structures.py
@@ -0,0 +1,10 @@
+class link:
+	innovation_num = -1
+	start_node = 0
+	end_node = 0
+	weight = 0.0
+	enabled = True
+
+class node:
+	id = 0
+	node_type = ""
