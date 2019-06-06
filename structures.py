fc7d0e90450b1ea60d8a7b5baa51f6bb94ec9828
Quentin Leon <qwolf1999@gmail.com>
2019-06-06 15:30:50 -0700

    nodetype

--- structures.py
+++ structures.py
@@ -1,14 +1,22 @@
-class link:
+from enum import Enum
+class Link:
 	innovation_num = -1
 	start_node = 0
 	end_node = 0
 	weight = 0.0
 	enabled = True
 
-class node:
-	id = 0
-	node_type = ""
+class NodeType(Enum):
+	NONE = 0
+	INPUT = 1
+	OUTPUT = 2
+	BIAS = 3
+	HIDDEN = 4
 
-class individual:
+class Node:
+	id = 0
+	node_type = NodeType.NONE
+
+class Individual:
 	nodes = []
 	links = []
